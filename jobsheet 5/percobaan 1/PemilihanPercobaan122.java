5151a09fcf1c8de586a5827bbfff0afdccee9d34
Octrian Adiluhung Tito Putra <octriantito@gmail.com>
2023-09-26 20:16:46 +0700

    Percobaan 2

--- jobsheet 5/percobaan 1/PemilihanPercobaan122.java
+++ jobsheet 5/percobaan 1/PemilihanPercobaan122.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PemilihanPercobaan123 {
+public class PemilihanPercobaan122 {
     public static void main(String[] args) {
         Scanner input23 = new Scanner(System.in);
 
