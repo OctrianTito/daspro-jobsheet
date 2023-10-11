5b83a52b9f22ea2fea2eca6282faa57adca5f0f0
Octrian Adiluhung Tito Putra <octriantito@gmail.com>
2023-10-11 11:22:58 +0700

    Percobaan 1

--- jobsheet 7/Percobaan 1/WhileKelipatan22.java
+++ jobsheet 7/Percobaan 1/WhileKelipatan22.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
-public class ForKelipatan22 {
-
+public class WhileKelipatan22 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int kelipatan, jumlah = 0, counter = 0;
         double rerata;
         System.out.print("Masukkan bilangan kelipatan (1-9) : ");
-        kelipatan = scan.nextInt();
-        for (int i = 1; i <= 50; i++) {
+        kelipatan = sc.nextInt();
+        int i = 1;
+        while(i <= 50) {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
             }
-        }
+            i++;
+        } 
         rerata = jumlah / counter;
 
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
