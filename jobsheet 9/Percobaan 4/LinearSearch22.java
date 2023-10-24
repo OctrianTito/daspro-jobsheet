17f8223abf2b4a75954773af09f6ed6bbc9645fd
Octrian Adiluhung Tito Putra <octriantito@gmail.com>
2023-10-24 19:22:08 +0700

    Modifikasi Percobaan 4

--- jobsheet 9/Percobaan 4/LinearSearch22.java
+++ jobsheet 9/Percobaan 4/LinearSearch22.java
@@ -1,16 +1,37 @@
+import java.util.Scanner;
 public class LinearSearch22 {
 
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
 
-        for(int i = 0; i < arrayInt.length; i++) {
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jumlahElemen = sc.nextInt();
+
+        int[] arrayInt = new int[jumlahElemen];
+
+        for (int i = 0; i < jumlahElemen; i++) {
+            System.out.print("Masukkan elemen array ke-" + (i) + " : ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        int key = sc.nextInt();
+
+        int hasil = -1; 
+
+        for (int i = 0; i < jumlahElemen; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi index ke-" + hasil);
+
+        if (hasil > -1) {
+            System.out.println("Key ada di posisi index ke-" + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan dalam array.");
+        }
+
+        sc.close();
     }
 }
