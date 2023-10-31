fd59a72401882cc2a5942284d4415836a97c5380
Octrian Adiluhung Tito Putra <octriantito@gmail.com>
2023-10-31 20:21:00 +0700

    Percobaan 3

---
+++ jobsheet 10/Percobaan 3/Numbers22.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Numbers22 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][5]; {
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + " : " + myNumbers[i].length);
+        }
+        }
+    }
+}
