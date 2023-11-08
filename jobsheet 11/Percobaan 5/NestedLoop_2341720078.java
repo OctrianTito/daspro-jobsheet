b012658bd09af576d2f7b6943edf5265b9580b22
Octrian Adiluhung Tito Putra <octriantito@gmail.com>
2023-11-08 09:43:38 +0700

    Modifikasi percobaan 5

--- jobsheet 11/Percobaan 5/NestedLoop_2341720078.java
+++ jobsheet 11/Percobaan 5/NestedLoop_2341720078.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class NestedLoop_2341720078 {
 
     public static void main(String[] args) {
@@ -8,7 +9,7 @@ public class NestedLoop_2341720078 {
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + (i + 1));
             for (int j = 0; j < temps[0].length; j++) {
-                System.out.print("Hari ke-" + (j+1) + " : ");
+                System.out.print("Hari ke-" + (j + 1) + " : ");
                 temps[i][j] = sc.nextDouble();
             }
             System.out.println();
@@ -17,10 +18,13 @@ public class NestedLoop_2341720078 {
         int kotaCounter = 1;
         for (double[] kota : temps) {
             System.out.print("Kota ke-" + kotaCounter + " : ");
+            double total = 0;
             for (double suhu : kota) {
                 System.out.print(suhu + " ");
+                total += suhu;
             }
-            System.out.println();
+            double rataRata = total / kota.length;
+            System.out.println("\nRata-rata suhu kota ke-" + kotaCounter + ": " + rataRata);
             kotaCounter++;
         }
     }
